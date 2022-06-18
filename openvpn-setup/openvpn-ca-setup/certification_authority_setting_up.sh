55c7c8bc3c6663dc67ece6466ce05270cecd6b58
C5323037 <oleksandr.bunin@sap.com>
2022-06-18 18:32:49 +0300

    fix

--- openvpn-setup/openvpn-ca-setup/certification_authority_setting_up.sh
+++ openvpn-setup/openvpn-ca-setup/certification_authority_setting_up.sh
@@ -10,22 +10,22 @@ then
 fi
 
 (. ../standard_functions.sh ) || \
-(echo "... # ../standard_functions were NOT imported ..." && exit 1)
+(echo "... # ../standard_functions were NOT imported ..." && return 1)
 
 echo_red "... #1 Prerequisites Installation ..."
-(. ./certification_authority_stages.sh install_prerequisites) || (echo_red "... # Could not execute #1 stage ..." && exit 1)
+(. ./certification_authority_stages.sh install_prerequisites) || (echo_red "... # Could not execute #1 stage ..." && return 1)
 echo_red "... #1 Done ..."
 
 echo_red "... #2 PKI Initialization ..."
-(. ./certification_authority_stages.sh init_pki) || (echo_red "... # Could not execute #2 stage ..." && exit 1)
+(. ./certification_authority_stages.sh init_pki) || (echo_red "... # Could not execute #2 stage ..." && return 1)
 echo_red "... #2 Done ..."
 
 echo_red "... #3 CA Creation ..."
-(. ./certification_authority_stages.sh create_certification_authority) || (echo_red "... # Could not execute #3 stage ..." && exit 1)
+(. ./certification_authority_stages.sh create_certification_authority) || (echo_red "... # Could not execute #3 stage ..." && return 1)
 echo_red "... #3 Done ..."
 
 echo_red "... #4 Certificates Distribution ..."
-(. ./certification_authority_stages.sh distribute_ca) || (echo_red "... # Could not execute #4 stage ..." && exit 1)
+(. ./certification_authority_stages.sh distribute_ca) || (echo_red "... # Could not execute #4 stage ..." && return 1)
 echo_red "... #4 Done ..."
 
 
