66794146c644889ea6d1b9bf36411fb671d8db28
C5323037 <oleksandr.bunin@sap.com>
2022-06-18 17:51:02 +0300

    fix

--- openvpn-setup/openvpn-ca-setup/certification_authority_sign.sh
+++ openvpn-setup/openvpn-ca-setup/certification_authority_sign.sh
@@ -11,13 +11,13 @@ fi
 (. ../standard_functions.sh ) \
 || (echo "... # ../standard_functions were NOT imported ..." && exit 1)
 
-usage() {                                 # Function: Print a help message.
+function usage() {                                 # Function: Print a help message.
   echo "Usage: $0 [ -c CERTIFICATE_PATH ] [ -t TYPE: {client, server} ] " 1>&2
 }
 
-exit_abnormal() {                         # Function: Exit with error.
+function exit_abnormal() {                         # Function: Exit with error.
   usage
-  return 1
+  exit 1
 }
 
 CERTIFICATE_PATH=""
@@ -47,16 +47,16 @@ done
 if [[ "${#CERTIFICATE_PATH}" -eq 0 ]]; then exit_abnormal ; fi
 if [[ "${#TYPE}" -eq 0 ]]; then exit_abnormal ; fi
 
-#EASY_RSA_DIR="/home/${USER}/easy-rsa" && \
-#cd "${EASY_RSA_DIR}" || (echo "Could not pass into ${EASY_RSA_DIR}" && exit 1)
-#
-#CERTIFICATION_NAME=''$(basename "${CERTIFICATE_PATH}" .crt)''
-#
-#(./easyrsa import-req "${CERTIFICATE_PATH}" "${CERTIFICATION_NAME}" && \
-#./easyrsa sign-req ${TYPE} "${CERTIFICATION_NAME}" && \
-#scp "./pki/issued/${CERTIFICATION_NAME}.crt" /tmp && \
-#scp "./pki/ca.crt" /tmp) || \
-#(echo_red "... # Could not sign the CSR ..." && exit 1)
-#cd "${PREV_PATH}" || echo_red "... # Could not pass into the ${PREV_PATH} ..."
-#echo_red "... Done ..."
+EASY_RSA_DIR="/home/${USER}/easy-rsa" && \
+cd "${EASY_RSA_DIR}" || (echo "Could not pass into ${EASY_RSA_DIR}" && exit 1)
+
+CERTIFICATION_NAME=''$(basename "${CERTIFICATE_PATH}" .crt)''
+
+(./easyrsa import-req "${CERTIFICATE_PATH}" "${CERTIFICATION_NAME}" && \
+./easyrsa sign-req ${TYPE} "${CERTIFICATION_NAME}" && \
+scp "./pki/issued/${CERTIFICATION_NAME}.crt" /tmp && \
+scp "./pki/ca.crt" /tmp) || \
+(echo_red "... # Could not sign the CSR ..." && exit 1)
+cd "${PREV_PATH}" || echo_red "... # Could not pass into the ${PREV_PATH} ..."
+echo_red "... Done ..."
 
