7dd87c8de99e4128b275003c9721b57c36922aa5
C5323037 <oleksandr.bunin@sap.com>
2022-07-03 20:03:10 +0300

    web-app-1/Dockerfile

---
+++ jenkins/setup_java_17.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -O jdk17.tag.gz https://download.oracle.com/java/17/archive/jdk-17.0.3.1_linux-x64_bin.tar.gz
+tar -xzvf jdk17.tar.gz
+find . -maxdepth 1 -type d -name '*jdk*' -exec mv {} jdk17 \;
+sudo ln -s "$(pwd)/jdk17/bin/java" /usr/local/bin/java17
+echo JDK_17="$(pwd)/jdk17" >> ~/.bashrc
+sudo chmod 777 ./switch-to-java17.sh
+sudo ln -s "$(pwd)/switch-to-java17.sh" /usr/local/bin/switch-java-17
---
+++ jenkins/setup_jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### 1 Installation
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+
+### 2 Run Jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
