e53fa546c9b8341d89b83440342a724773910eab
C5323037 <oleksandr.bunin@sap.com>
2022-07-03 20:54:23 +0300

    setup_mvn_3.8.3.sh

--- jenkins/java-17/setup_java_17.sh
+++ jenkins/java-17/setup_java_17.sh
@@ -3,7 +3,7 @@
 wget -O jdk17.tar.gz https://download.oracle.com/java/17/archive/jdk-17.0.3.1_linux-x64_bin.tar.gz
 tar -xzvf jdk17.tar.gz
 find . -maxdepth 1 -type d -name '*jdk*' -exec mv {} jdk17 \;
-sudo ln -s "$(pwd)/jdk17/bin/java" /usr/local/bin/java17
+sudo ln -sf "$(pwd)/jdk17/bin/java" /usr/local/bin/java17
 echo JDK_17="$(pwd)/jdk17" >> ~/.bashrc
 sudo chmod 777 ./switch-to-java-17.sh
-sudo ln -s "$(pwd)/switch-to-java-17.sh" /usr/local/bin/switch-java-17
+sudo ln -sf "$(pwd)/switch-to-java-17.sh" /usr/local/bin/switch-java-17
