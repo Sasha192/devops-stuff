54b21049af4367139f1314f05c0bf17551ca667c
C5323037 <oleksandr.bunin@sap.com>
2022-07-04 17:25:15 +0300

    setup_jenkins.sh

--- jenkins/setup_jenkins.sh
+++ jenkins/setup_jenkins.sh
@@ -6,9 +6,21 @@ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sour
 sudo apt update
 sudo apt install jenkins
 
+# JENKINS_HOME;
+mkdir "/home/jenkins"
+sudo chown jenkins:jenkins "/home/jenkins"
+sudo usermod -d "/home/jenkins" jenkins
+sudo ln -s "/home/jenkins" /var/lib/jenkins
+sudo chown jenkins /var/lib/jenkins
+sudo sed -i -e "/JENKINS_HOME=/ s/=.*/=/home/jenkins/" /etc/default/jenkins
+
+sudo su jenkins
+
 ### 2 Run Jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+#sudo systemctl restart jenkins.service
+#sudo systemctl enable jenkins.service
+
+
 
 ### 3 initialAdminSecret
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+#sudo cat /var/lib/jenkins/secrets/initialAdminPassword
