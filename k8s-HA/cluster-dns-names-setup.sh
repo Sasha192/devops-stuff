e667beff4b5df2214a850d3b32500990b33acfb3
C5323037 <oleksandr.bunin@sap.com>
2022-07-06 15:53:17 +0300

    k8s-HA-control-plane

---
+++ k8s-HA/cluster-dns-names-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SUBNET_ADDR=192.168.31.0/24
+if ! [ $# -eq 0 ]; then
+  SUBNET_ADDR=$1
+fi
+ubuntu_names=$(sudo nmap -sP $SUBNET_ADDR | grep 'ubuntu*' | awk '{print $5, $6}' | tr -d '()')
+sudo sed -i.bak '/ubuntu*/d' /etc/hosts
+for ubuntu_i in "${ubuntu_names[@]}"; do
+ echo -e "$ubuntu_i\n$(sudo cat /etc/hosts)" > /etc/hosts
+done
+
+K8S_CONTROL_PLANE_HOST=$(dig ubuntu1 +short)
+echo -e "$K8S_CONTROL_PLANE_HOST k8s-control-plane\n$(sudo cat /etc/hosts)" > /etc/hosts
+
