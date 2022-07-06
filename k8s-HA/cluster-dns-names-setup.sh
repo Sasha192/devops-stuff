6ab7b2946aa866ecb2d0d985941ee65837566aaf
C5323037 <oleksandr.bunin@sap.com>
2022-07-06 16:03:57 +0300

    k8s-HA-control-plane

--- k8s-HA/cluster-dns-names-setup.sh
+++ k8s-HA/cluster-dns-names-setup.sh
@@ -7,14 +7,14 @@ fi
 ubuntu_names=$(sudo nmap -sP $SUBNET_ADDR | grep 'ubuntu*' | awk '{print $5, $6}' | tr -d '()')
 sudo sed -i.bak '/ubuntu*/d' /etc/hosts
 for ubuntu_i in "${ubuntu_names[@]}"; do
- echo -e "$ubuntu_i\n$(sudo cat /etc/hosts)" > /etc/hosts
+ echo -e "$ubuntu_i\n$(sudo cat /etc/hosts)" | sudo tee /etc/hosts
 done
 
 K8S_CONTROL_PLANE_HOST=$(dig ubuntu1 +short)
-echo -e "$K8S_CONTROL_PLANE_HOST k8s-control-plane\n$(sudo cat /etc/hosts)" > /etc/hosts
+echo -e "$K8S_CONTROL_PLANE_HOST k8s-control-plane\n$(sudo cat /etc/hosts)" | sudo tee /etc/hosts
 
 K8S_CONTROL_PLANE_HOST=$(dig ubuntu1 +short)
 
 if [[ $(hostname) =~ ubuntu* ]]; then
-  echo -e "127.0.1.1 $(hostname)\n$(sudo cat /etc/hosts)" > /etc/hosts
+  sudo echo -e "127.0.1.1 $(hostname)\n$(sudo cat /etc/hosts)" | sudo tee /etc/hosts
 fi
