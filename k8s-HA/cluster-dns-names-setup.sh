d1cc48835032d4363d82d082f0834b1079eeef48
C5323037 <oleksandr.bunin@sap.com>
2022-07-06 16:11:28 +0300

    k8s-HA-control-plane

--- k8s-HA/cluster-dns-names-setup.sh
+++ k8s-HA/cluster-dns-names-setup.sh
@@ -4,7 +4,7 @@ SUBNET_ADDR=192.168.31.0/24
 if ! [ $# -eq 0 ]; then
   SUBNET_ADDR=$1
 fi
-ubuntu_names=$(sudo nmap -sP $SUBNET_ADDR | grep 'ubuntu*' | awk '{print $5, $6}' | tr -d '()')
+ubuntu_names=$(sudo nmap -sP "$SUBNET_ADDR" | grep 'ubuntu*' | awk '{print $5, $6}' | tr -d '()')
 sudo sed -i.bak '/ubuntu*/d' /etc/hosts
 for ubuntu_i in "${ubuntu_names[@]}"; do
  echo -e "$ubuntu_i\n$(sudo cat /etc/hosts)" | sudo tee /etc/hosts
